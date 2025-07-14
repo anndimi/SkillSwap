01504d3329605461faf790c16d65d2941841f57e
Sujith Unnikrishnan <sujith.sopanam@hotmail.com>
2025-07-14 15:54:43 +0200

    adding missing form fields

--- skillswapproject/skillswapapp/forms.py
+++ skillswapproject/skillswapapp/forms.py
@@ -1,7 +1,7 @@
 from django import forms
-from .models import Skill
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import UserProfile
+from .models import Skill, UserProfile
 
 class SkillForm(forms.ModelForm):
     class Meta:
@@ -17,3 +17,19 @@ class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         fields = ['profile_picture', 'bio']
+
+class CustomSignupForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    fullname = forms.CharField(label='Full Name', max_length=150, required=True)
+    class Meta:
+        model = User
+        fields = ['username', 'fullname', 'email', 'password1', 'password2']
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data['email']
+        full = self.cleaned_data['fullname'].strip().split(' ', 1)
+        user.first_name = full[0]
+        user.last_name = full[1] if len(full) > 1 else ''
+        if commit:
+            user.save()
+        return user
